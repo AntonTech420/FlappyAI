9e63096ed9e50e6c00951f2de19b4c90f1d3149b
Troy <shutman18@gmail.com>
2021-09-01 11:45:29 +1200

    PipeHandler

---
+++ FlappyAI/Assets/Scripts/Bird.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : Agent
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private PipeHandler pipeHandler = null;
+    [SerializeField] private Transform bodyTransform = null;
+
+    [Header("Settings")]
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float maxVelocityMagnitude = 5f;
+
+    private Vector3 startingPos;
+
+    public override void Initialize() {
+        startingPos = transform.position;
+    }
+
+    public override void OnEpisodeBegin() {
+        transform.position = startingPos;
+        rb.velocity = Vector3.zero;
+
+        pipeHandler.ResetPipes();
+    }
+
+    public override void OnActionReceived(float[] vectorAction) {
+        AddReward(0.1f);
+
+        if (Mathf.FloorToInt(vectorAction[0]) !=1) { return; }
+
+        Jump();
+    }
+
+    private void Jump() {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocityMagnitude);
+    }
+}
